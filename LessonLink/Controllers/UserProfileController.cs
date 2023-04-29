6d9fc00f074bb28fcecb19b6facf7252a8e18f71
Grayson Smith <graysonsmith91@gmail.com>
2023-04-29 10:14:49 -0500

    set up firebase and added to project

---
+++ LessonLink/Controllers/UserProfileController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using LessonLink.Repositories;
+using LessonLink.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace LessonLink.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileRepository _userProfileRepository;
+        public UserProfileController(IUserProfileRepository userProfileRepository)
+        {
+            _userProfileRepository = userProfileRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_userProfileRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var video = _userProfileRepository.GetById(id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+            return Ok(video);
+        }
+
+        [HttpGet("Firebase/{firebaseUserId}")]
+        public IActionResult GetByFirebaseUserId(string firebaseUserId)
+        {
+            var userProfile = _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok(userProfile);
+        }
+
+        [HttpGet("Videos/{id}")]
+        public IActionResult GetUserByIdWithVideos(int id)
+        {
+            var userProfile = _userProfileRepository.GetUserByIdWithVideos(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok(userProfile);
+        }
+
+        [HttpPost]
+        public IActionResult Post(UserProfile userProfile)
+        {
+            userProfile.DateCreated = DateTime.Now;
+            _userProfileRepository.Add(userProfile);
+            return CreatedAtAction("Get", new { id = userProfile.Id }, userProfile);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UserProfile userProfile)
+        {
+            if (id != userProfile.Id)
+            {
+                return BadRequest();
+            }
+
+            _userProfileRepository.Update(userProfile);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userProfileRepository.Delete(id);
+            return NoContent();
+        }
+
+        [HttpGet("Me")]
+        public IActionResult Me()
+        {
+            var userProfile = GetCurrentUserProfile();
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userProfile);
+        }
+
+        [HttpGet("DoesUserExist/{firebaseUserId}")]
+        public IActionResult DoesUserExist(string firebaseUserId)
+        {
+            var userProfile = _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        //[HttpPost("Register")]
+        //public IActionResult Register(UserProfile userProfile)
+        //{
+        //    // All newly registered users start out as a "user" user type (i.e. they are not admins)
+        //    // userProfile.UserTypeId = UserType.USER_TYPE_ID;
+        //    userProfile.DateCreated = DateTime.Now;
+        //    _userProfileRepository.Add(userProfile);
+        //    return CreatedAtAction(
+        //        nameof(GetByFirebaseUserId), new { firebaseUserId = userProfile.FirebaseUserId }, userProfile);
+        //}
+
+        private UserProfile GetCurrentUserProfile()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+        }
+    }
+}
---
+++ LessonLink/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Streamish.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile userProfile);
+        void Delete(int id);
+        System.Collections.Generic.List<UserProfile> GetAll();
+        UserProfile GetByFirebaseUserId(string firebaseUserId);
+        UserProfile GetById(int id);
+        UserProfile GetUserByIdWithVideos(int id);
+        void Update(UserProfile userProfile);
+    }
+}
