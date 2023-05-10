57cb3bda676f827793c597f486d5b2d0cc2af522
Grayson Smith <graysonsmith91@gmail.com>
2023-05-09 19:38:11 -0500

    fixed home page active student count and lessons this week count

--- LessonLink/client/src/components/Home.js
+++ LessonLink/client/src/components/Home.js
@@ -1,21 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { me } from "../modules/authManager";
+import { getStudentsByTeacherId } from "../modules/studentManager";
+import { getLessonsByTeacherId } from "../modules/lessonManager";
 
-export default function Hello() {
-    const [user, setUser] = useState(null);
+export default function Home({ userProfile }) {
+    const [students, setStudents] = useState([]);
+    const [lessons, setLessons] = useState([]);
+    const [filteredLessons, setFilteredLessons] = useState([]);
+
+    const today = new Date().toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
+    const todayCheck = new Date();
+    const nextWeek = new Date();
+    nextWeek.setDate(nextWeek.getDate() + 7);
 
     useEffect(() => {
-        me().then(setUser)
-    }, []);
+        if (userProfile) {
+            getStudentsByTeacherId(userProfile.id).then((students) => {
+                setStudents(students);
+            });
+            getLessonsByTeacherId(userProfile.id).then((lessons) => {
+                setLessons(lessons);
+            });
+        }
+    }, [userProfile]);
+
+    useEffect(() => {
+        const filtered = lessons.filter((lesson) => {
+            const lessonDate = new Date(lesson.dateTime);
+            return lessonDate <= nextWeek && lessonDate >= todayCheck;
+        });
+        setFilteredLessons(filtered);
+    }, [lessons]);
 
     return (
-        <span style={{
-            position: "fixed",
-            left: 0,
-            right: 0,
-            top: "35%",
-            marginTop: "-0.5rem",
-            textAlign: "center",
-        }}>Hello, {user?.fullName}</span>
+        <>
+            <div style={{
+                position: "fixed",
+                left: 0,
+                right: 0,
+                top: "35%",
+                marginTop: "-0.5rem",
+                textAlign: "center",
+            }}>
+                <span style={{ fontSize: "2rem" }}>Hello, {userProfile?.fullName}</span>
+                <div style={{ fontSize: "1rem" }}>
+                    <h1>Today is {today}</h1>
+                </div>
+                <div style={{ fontSize: "1.5rem" }}>Active Students: {students?.length}</div>
+                <div style={{ fontSize: "1.5rem" }}>Lessons This Week: {filteredLessons?.length}</div>
+            </div>
+        </>
     );
 }
--- LessonLink/client/src/components/Teacher.js
+++ LessonLink/client/src/components/Teacher.js
@@ -1,7 +1,10 @@
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { me } from "../modules/authManager";
 
-export default function Teacher({ teacher }) {
+export default function Teacher({ teacher, userProfile }) {
     const navigate = useNavigate();
+
     return (
         <tr key={teacher.id} style={{ cursor: 'pointer' }} onClick={() => navigate(`/teachers/details/${teacher.id}`)}>
             <td>{teacher.fullName}</td>
@@ -9,8 +12,12 @@ export default function Teacher({ teacher }) {
             <td>{teacher.phone}</td>
             <td>{teacher.userTypeId === 1 ? "✔️" : ""}</td>
             <td className="text-end">
-                <Link to={`Edit/${teacher.id}`} className="edit-button btn btn-outline-dark btn-sm" onClick={(e) => e.stopPropagation()}>Edit</Link>
-                <Link to={`Delete/${teacher.id}`} className="del-button btn btn-outline-danger btn-sm" onClick={(e) => e.stopPropagation()}>Delete</Link>
+                {userProfile?.id === 1 && (
+                    <>
+                        <Link to={`Edit/${teacher.id}`} className="edit-button btn btn-outline-dark btn-sm" onClick={(e) => e.stopPropagation()}>Edit</Link>
+                        <Link to={`Delete/${teacher.id}`} className="del-button btn btn-outline-danger btn-sm" onClick={(e) => e.stopPropagation()}>Delete</Link>
+                    </>
+                )}
             </td>
         </tr>
     );
